fd6b99da2dc9c3ed1f2c974140f67b834b5c91d0
vinayak dubey <vinayak.dubey2019@vitstudent.ac.in>
2021-05-24 01:10:37 +0530

    tried file upload

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ let path = require('path');
 let bodyParser = require('body-parser');
 let firebase = require('firebase');
 let dotenv = require('dotenv').config();
+const multer = require('multer');
+var fs = require('fs')
+
 
 let app = express();
 
@@ -17,6 +20,9 @@ let firebaseConfig = {
     measurementId: process.env.measurementId
 };
 firebase.initializeApp(firebaseConfig);
+
+// const bucket = firebase.bucket()
+var storage = firebase.storage()
 
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -102,6 +108,62 @@ app.post("/signin", function(req,res){
         })
 });
 
+const uploads = multer({
+    storage: Multer.memoryStorage(),
+    limits: {
+      fileSize: 5 * 1024 * 1024 // no larger than 5mb, you can change as needed.
+    }
+  });
+
+app.post('/upload-blog', uploads.single("fancy-file"), async (req, res) => {
+    console.log('Upload Image');
+    console.log(req.body)
+    let file = req.file;
+    if (file) {
+        uploadImageToStorage(file).then((success) => {
+        res.status(200).send({
+                status: 'success'
+            });
+            }).catch((error) => {
+                console.error(error);
+        });
+    }
+  });
+  
 app.listen(3000,()=>{
     console.log('server at http://localhost:3000');
 });
+
+
+/**
+ * Upload the image file to Google Storage
+ * @param {File} file object that will be uploaded to Google Storage
+ */
+ const uploadImageToStorage = (file) => {
+    return new Promise((resolve, reject) => {
+      if (!file) {
+        reject('No image file');
+      }
+      let newFileName = `${file.originalname}_${Date.now()}`;
+  
+      let fileUpload = storage.file(newFileName);
+  
+      const blobStream = fileUpload.createWriteStream({
+        metadata: {
+          contentType: file.mimetype
+        }
+      });
+  
+      blobStream.on('error', (error) => {
+        reject('Something is wrong! Unable to upload at the moment.');
+      });
+  
+      blobStream.on('finish', () => {
+        // The public URL can be used to directly access the file via HTTP.
+        const url = format(`https://storage.googleapis.com/${storage.name}/${fileUpload.name}`);
+        resolve(url);
+      });
+  
+      blobStream.end(file.buffer);
+    });
+  }
