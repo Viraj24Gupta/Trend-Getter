6f5b5f64c6d53318836e9c0f364c9eec5cda18f1
Viraj24Gupta <viraj24Gupta@gmailcom>
2021-05-28 03:45:33 +0530

    .

--- app.js
+++ app.js
@@ -24,6 +24,9 @@ firebase.initializeApp(firebaseConfig);
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname,'./')));
+app.set('views', path.join(__dirname, 'templates'));
+app.set('view engine', 'ejs');
+
 
 app.get('/', function(req,res){
     console.log('GET signin');
